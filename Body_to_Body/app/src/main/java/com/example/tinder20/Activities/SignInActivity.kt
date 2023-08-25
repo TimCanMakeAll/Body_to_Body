29649d4dd97fb142bdd6e011dbdf842ed01790a1
Tim1 <timofeiverbickii4@gmail.com>
2023-08-26 01:42:00 +0300

    added dialog for changing password (skeleton)

--- Body_to_Body/app/src/main/java/com/example/tinder20/Activities/SignInActivity.kt
+++ Body_to_Body/app/src/main/java/com/example/tinder20/Activities/SignInActivity.kt
@@ -1,9 +1,16 @@
 package com.example.tinder20.Activities
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import android.util.Log
+import android.view.Window
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.graphics.toColor
 import com.example.tinder20.R
@@ -70,6 +77,21 @@ class SignInActivity : AppCompatActivity() {
                     binding.layoutEmailInput.setHelperTextColor(this.getColorStateList(R.color.redError))
                 }
             binding.layoutEmailInput.isHelperTextEnabled = false
+        }
+
+        binding.tvForgotPassword.setOnClickListener {
+            val dialogBinding = layoutInflater.inflate(R.layout.resend_email_dialog, null)
+            val myDialog = Dialog(this)
+            myDialog.setContentView(dialogBinding)
+
+            myDialog.setCancelable(true)
+            myDialog.window?.setLayout(((resources.displayMetrics.widthPixels*0.90).toInt()), (resources.displayMetrics.widthPixels*1))
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+
+            dialogBinding.findViewById<ImageView>(R.id.img_btn_cancel).setOnClickListener {
+                myDialog.dismiss()
+            }
         }
 
         binding.btnBackToRegistration.setOnClickListener {
